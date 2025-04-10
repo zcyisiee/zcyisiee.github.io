718b120dfba274269eb099b21e9f4b39160e61d0
zcyisiee <202211079182@mail.bnu.edu.cn>
2025-04-11 00:04:49 +0800

    修复了数学公式有时渲染不成功的问题（自定义math.js），更新了chirpy使用记录。

---
+++ assets/js/math.js
@@ -0,0 +1,31 @@
+// 创建文件: /assets/js/math.js
+window.MathJax = {
+  tex: {
+    inlineMath: [
+      ['$', '$'],
+      ['\\(', '\\)'],
+    ],
+    displayMath: [
+      ['$$', '$$'],
+      ['\\[', '\\]'],
+    ],
+    processEscapes: true,
+    processEnvironments: true,
+    processRefs: true,
+    tags: 'ams', // 'none' 或 'ams' 或 'all'
+    tagSide: 'right',
+    tagIndent: '0.8em',
+    useLabelIds: true,
+    multlineWidth: '85%',
+  },
+  options: {
+    renderActions: {
+      addMenu: [0, '', ''],
+    },
+    ignoreHtmlClass: 'tex2jax_ignore',
+    processHtmlClass: 'tex2jax_process',
+  },
+  svg: {
+    fontCache: 'global', // 提高性能
+  },
+};
